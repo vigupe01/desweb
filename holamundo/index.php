24b685a559887251a7d9e8e83ce8622811c343f1
unknown <2daw@winmac3.alumnado.flo>
2015-10-02 10:59:14 +0200

    Inicio de Repositorio

---
+++ holamundo/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Mi PRIMERA PAGINA WEB</title>
+	</head>
+	<body>
+		Mi primer codigo HTML
+		<br>
+		<?php
+		echo "Esto abarca multiple líneas. Los saltos de linea tambien se mostraran";
+		?>
+	</body>
+</html>
---
+++ tiposvariables/variables.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+			$frase="Paco es un campeon";
+
+			echo $frase;
+		?>
+	</body>
+</html>
